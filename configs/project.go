e81bbbe10d31d820103060509de465b8db9b31db
Igor Grillo Peternella <igor.feq@gmail.com>
2020-02-29 23:08:02 -0300

    Added config and tests packages

---
+++ configs/project.go
@@ -0,0 +1,3 @@
+package config
+
+const ConfigVar int = 1
---
+++ tests/cmd/tictacgoe/main_test.go
@@ -0,0 +1,8 @@
+package tictacgoe_test
+
+import "testing"
+
+// Sample test
+func TestExample_ShouldNotRaiseErrors(t *testing.T) {
+	//t.Fail("Ops!")
+}
