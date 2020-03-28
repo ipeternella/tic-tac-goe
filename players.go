65d8e04a5a0cfa25680651b7becd22313324a4c2
Igor Grillo Peternella <igor.feq@gmail.com>
2020-03-28 11:16:14 -0300

    Created initial function to validate players moves. Added tests.

--- players.go
+++ players.go
@@ -2,14 +2,46 @@ package tictacgoe
 
 import (
 	"bufio"
+	"fmt"
 	"io"
 )
 
 // Stores user input bytes that from stdin, in a buffer that is read
 func GetUserInput(stdIn io.Reader) string {
+
 	// creates an input buffer that receives []byte streams from stdin
 	inputBuffer := bufio.NewReader(stdIn)
 	text, _ := inputBuffer.ReadString('\n')
 
 	return text
+
+}
+
+// Asserts that a user input is within the board size range
+// and that the field spot is not already taken by another mark
+func IsUserInputValid(fieldPosition int, board *Board) (bool, string) {
+
+	// board constraints
+	maxAllowedValue := len(board.fields) * len(board.fields) // assumes first field starts at 1
+	minAllowedValue := 1
+
+	// outcomes
+	gameRejectionOutput := ""
+	evaluationResult := false
+
+	// assertion if the spot is taken
+	if false {
+		evaluationResult = true
+	}
+
+	if fieldPosition < minAllowedValue {
+		gameRejectionOutput = fmt.Sprintf("Oops! The lowest allowed spot value for your move is %d!", minAllowedValue)
+	}
+
+	if fieldPosition > maxAllowedValue {
+		gameRejectionOutput = fmt.Sprintf("Oops! The highest allowed spot value for your move is %d!", maxAllowedValue)
+	}
+
+	return evaluationResult, gameRejectionOutput
+
 }
--- players_test.go
+++ players_test.go
@@ -6,6 +6,7 @@ import (
 )
 
 func TestGetUserInputFromStdIn(t *testing.T) {
+
 	// expected values
 	expectedInput := "Hello world!"
 
@@ -22,5 +23,40 @@ func TestGetUserInputFromStdIn(t *testing.T) {
 
 	if input != expectedInput {
 		t.Errorf("Unexpected input. actual: %s, expected: %s", input, expectedInput)
+	}
+
+}
+
+func TestIsUserInputValidMinThresholdExceeded(t *testing.T) {
+	board := CreateGameBoard(3) // 3x3 board --> min value is 1
+	expectedOutput := "Oops! The lowest allowed spot value for your move is 1!"
+
+	// function invocation
+	isValid, output := IsUserInputValid(0, board)
+
+	if isValid {
+		t.Errorf("Expected input should be false. actual: %t, expected: %t", isValid, false)
+	}
+
+	if output != expectedOutput {
+		t.Errorf("Game output is different than expected. actual: %v, expected: %v", output, expectedOutput)
+	}
+}
+
+func TestIsUserInputValidMaxThresholdExceeded(t *testing.T) {
+	board := CreateGameBoard(5) // 5 x 5 board --> max value is 25
+	expectedOutput := "Oops! The highest allowed spot value for your move is 25!"
+
+	// function invocation
+	isValid, output := IsUserInputValid(26, board)
+
+	// result must not be valid
+	if isValid {
+		t.Errorf("Expected input should be false. actual: %t, expected: %t", isValid, false)
+	}
+
+	// output assertion
+	if output != expectedOutput {
+		t.Errorf("Game output is different than expected. actual: %v, expected: %v", output, expectedOutput)
 	}
 }
