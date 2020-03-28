d4629c0bd372b12cb7802344c2ec3e82952510ae
Igor Grillo Peternella <igor.feq@gmail.com>
2020-03-28 20:49:06 -0300

    Added to print board with some extra display logic

--- internal/app/tictacgoe/repl/print.go
+++ internal/app/tictacgoe/repl/print.go
@@ -1,2 +1,27 @@
 // Prints the game state to the players
 package repl
+
+import (
+	"fmt"
+	"time"
+
+	"github.com/IgooorGP/tic-tac-goe/internal/app/tictacgoe/gamelogic"
+	"github.com/IgooorGP/tic-tac-goe/internal/app/tictacgoe/settings"
+)
+
+// Writes to terminal screen buffer
+func DisplayScreenMessage(message string, newline bool) {
+	if newline {
+		fmt.Println(message)
+	}
+
+	fmt.Println(message)
+}
+
+func DisplayBoardWithSpaces(board *gamelogic.Board) {
+	time.Sleep(300 * time.Millisecond) // waits a little bit before writing board to screen buffer
+	DisplayScreenMessage("\n", false)
+
+	gamelogic.PrintBoard(board, settings.BoardHorizontalSeparator)
+	DisplayScreenMessage("\n", false)
+}
