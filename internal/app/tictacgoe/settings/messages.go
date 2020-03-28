26689ae5afc25b6a591a6c3a47ed361cd28703cf
Igor Grillo Peternella <igor.feq@gmail.com>
2020-03-28 20:47:18 -0300

    Added user display messages to settings package

---
+++ internal/app/tictacgoe/settings/messages.go
@@ -0,0 +1,27 @@
+// messages that are displayed to the user
+package settings
+
+// Intro
+const WelcomeMsg string = `
++ - - - - - - - - - - - - - - - - - - - - - - - - - - - - - +  
+|                       WELCOME :)!                         |
+|    TT | II | CC                                           |
+|   --------------    ____   ____    ____                   |
+|    TT | AA | CC       | i c  | a c  | o e                 |
+|   --------------              Terminal 1.0                |
+|    GG | OO | EE                                           |
+|                                                           |
+|   This program is licensed under the MIT License.         |
+|   >> Press ANY KEY to begin                               |
++ - - - - - - - - - - - - - - - - - - - - - - - - - - - - - +
+`
+
+// Before the match
+var MatchAboutToStartMsg string = "Match's about to start in a %d x %d board. Press ANY KEY when ready!"
+
+// During the match
+var InitialBoardMsg string = "Here's a brand new board just for you..."
+var AskNextMoveMsg string = "Mark the field at the desired position with a number:"
+
+// Exiting the match
+var ByeByeMsg string = "Thanks for Playing! Come again any time! The game will end in a few seconds..."
