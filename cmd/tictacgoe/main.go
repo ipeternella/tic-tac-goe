6a5023bd58f2ab0bc2952e4b5d6c93fb8937354c
Igor Grillo Peternella <igor.feq@gmail.com>
2020-03-28 20:49:42 -0300

    Created main game loop. Added its invocation in the main package.

--- cmd/tictacgoe/main.go
+++ cmd/tictacgoe/main.go
@@ -2,13 +2,7 @@
 package main
 
 import (
-	"os"
-	"strconv"
-
 	"github.com/IgooorGP/tic-tac-goe/internal/app/tictacgoe/repl"
-
-	"github.com/IgooorGP/tic-tac-goe/internal/app/tictacgoe/gamelogic"
-
 	"github.com/IgooorGP/tic-tac-goe/internal/app/tictacgoe/settings"
 
 	"github.com/rs/zerolog/log"
@@ -18,26 +12,8 @@ import (
 func main() {
 	// sets the application up
 	settings.SetupApplication()
-
 	log.Debug().Msgf("Starting the game...")
 
-	gameBoard := gamelogic.CreateGameBoard(settings.BoardSize)
-	gamelogic.PrintBoard(gameBoard, settings.BoardHorizontalSeparator)
-
-	userInput := repl.GetUserInput(os.Stdin)
-	userFieldPositionInput, intConversionError := strconv.Atoi(userInput)
-
-	if intConversionError != nil {
-		log.Fatal().Msg("Input was not an integer!")
-	}
-
-	if isValid, rejectionMsg := gamelogic.IsUserInputValid(userFieldPositionInput, gameBoard); !isValid {
-		log.Fatal().Msg(rejectionMsg)
-	}
-
-	// updates game and re-prints the board
-	gamelogic.UpdateGameState("IG", userFieldPositionInput, gameBoard)
-	gamelogic.PrintBoard(gameBoard, settings.BoardHorizontalSeparator)
-
+	repl.StartGameLoop()
 	log.Debug().Msgf("Closing the game application...")
 }
--- internal/app/tictacgoe/repl/loop.go
+++ internal/app/tictacgoe/repl/loop.go
@@ -1,2 +1,36 @@
 // contains main game loop
 package repl
+
+import (
+	"fmt"
+	"strconv"
+
+	"github.com/IgooorGP/tic-tac-goe/internal/app/tictacgoe/gamelogic"
+
+	"github.com/IgooorGP/tic-tac-goe/internal/app/tictacgoe/settings"
+)
+
+// Runs the game loop until the user wants to quit
+func StartGameLoop() {
+
+	AskUserWithoutOptions(settings.WelcomeMsg, false)
+	AskUserWithoutOptions(fmt.Sprintf(settings.MatchAboutToStartMsg, settings.BoardSize, settings.BoardSize), false)
+	DisplayScreenMessage(settings.InitialBoardMsg, false)
+	gameBoard := gamelogic.CreateGameBoard(settings.BoardSize)
+
+	// forever game loop
+	for {
+		DisplayBoardWithSpaces(gameBoard)
+		rawUserInput := AskUserWithoutOptions(settings.AskNextMoveMsg, false) // TODO: validate inputs
+
+		// checks if user wants to quit
+		if rawUserInput == settings.QuitMark {
+			break
+		}
+
+		userFieldPositionInput, _ := strconv.Atoi(rawUserInput)
+		gamelogic.UpdateGameState(settings.Player1Mark, userFieldPositionInput, gameBoard) // TODO: asserts game over
+	}
+
+	DisplayScreenMessage(settings.ByeByeMsg, true)
+}
